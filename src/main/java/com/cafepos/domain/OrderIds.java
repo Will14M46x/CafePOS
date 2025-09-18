775e1380d6c471986961e10adfbf39031f6d39d3
william <williamwalsh483@gmail.com>
2025-09-18 10:45:48 +0100

    fuck this part 3

--- src/main/java/com/cafepos/domain/OrderIds.java
+++ src/main/java/com/cafepos/domain/OrderIds.java
@@ -1,6 +1,12 @@
 package com.cafepos.domain;
 
-public interface OrderIds {
-    String id();
-    String items();
+import java.util.concurrent.atomic.AtomicLong;
+
+public class OrderIds {
+    private static final AtomicLong number = new AtomicLong(1000);
+    private OrderIds() {}
+
+    public static long next() {
+        return number.incrementAndGet();
+    }
 }
