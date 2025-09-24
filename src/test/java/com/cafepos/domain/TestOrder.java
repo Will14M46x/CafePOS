c6f52adc550e43ee968dd1571587564010dcac2d
william <williamwalsh483@gmail.com>
2025-09-24 10:15:56 +0100

    week 03 lab done

--- src/test/java/com/cafepos/domain/TestOrder.java
+++ src/test/java/com/cafepos/domain/TestOrder.java
@@ -2,9 +2,11 @@ package com.cafepos.domain;
 
 import com.cafepos.catalog.SimpleProduct;
 import com.cafepos.common.Money;
+import com.cafepos.payment.PaymentStrategy;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestOrder {
     @Test
@@ -17,5 +19,16 @@ public class TestOrder {
         assertEquals(Money.of(8.50), o.subtotal());
         assertEquals(Money.of(0.85), o.taxAtPercent(10));
         assertEquals(Money.of(9.35), o.totalWithTax(10));
+    }
+
+    @Test
+    public void payment_strategy_called(){
+        var p = new SimpleProduct("A","A", Money.of(5));
+        var order = new Order(42);
+        order.addItem(new LineItem(p, 1));
+        final boolean[] called = {false};
+        PaymentStrategy fake = o -> called[0] = true;
+        order.pay(fake);
+        assertTrue("Payment strategy should be called",called[0]);
     }
 }
