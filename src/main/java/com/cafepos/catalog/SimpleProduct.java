c0a2110b320b4e79e9b33fba81c9236f417eff77
WilliamWalsh <22345302@studentmail.ul.ie>
2025-10-06 16:52:42 +0100

    Lab not done

--- src/main/java/com/cafepos/catalog/SimpleProduct.java
+++ src/main/java/com/cafepos/catalog/SimpleProduct.java
@@ -1,7 +1,8 @@
 package com.cafepos.catalog;
 import com.cafepos.common.Money;
+import com.cafepos.decorator.Priced;
 
-public final class SimpleProduct implements Product {
+public final class SimpleProduct implements Product, Priced {
     private final String id;
     private final String name;
     private final Money basePrice;
@@ -18,4 +19,7 @@ public final class SimpleProduct implements Product {
 
     @Override
     public Money basePrice() { return basePrice; }
+
+    @Override
+    public Money price() { return basePrice; }
 }
